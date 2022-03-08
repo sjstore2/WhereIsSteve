94732d8e8ef46ba9a23f2570deffb4e43de5605f
Scott <Scott.storey@gmail.com>
2022-03-08 08:41:12 +0000

    Update Welcome.js

--- Welcome.js
+++ Welcome.js
@@ -1,5 +1,4 @@
-
-const greet = ["Yes", "All signs point to yes", "maybe even two at the same time"];
+const greet = ["Steve is in a meeting", "All signs point to Steve being in a meeting", "Steve is in two meetings at the same time", "Steve is in a meeting that should have been an email", "Steve wishes he wasn't in a meeting on WebEx", "Steve is on a zoom call"];
 
 const random = Math.floor(Math.random() * greet.length);
 
