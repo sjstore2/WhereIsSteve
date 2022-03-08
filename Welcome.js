501e06603e8f98691db1ff20805da37b0ea13caa
Scott <Scott.storey@gmail.com>
2022-03-08 08:20:35 +0000

    Create Welcome.js

---
+++ Welcome.js
@@ -0,0 +1,7 @@
+
+const greet = ["Yes", "All signs point to yes", "maybe even two at the same time"];
+
+const random = Math.floor(Math.random() * greet.length);
+
+var show = document.getElementById('message');
+show.textContent = random
